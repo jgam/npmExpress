f362348bb9b6c90f11906c7a9da37bd7a3fa754e
jgam <jgam@alumni.nd.edu>
2020-06-25 19:28:43 +0900

    [express] set up before the pug

--- app.js
+++ app.js
@@ -5,21 +5,26 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 
 //router imports
-import userRouter from "./routers/userRouter.js";
-import videoRouter from "./routers/videoRouter.js";
-import globalRouter from "./routers/globalRouter.js";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes.js";
 
 const app = express();
 
 app.use(cookieParser());
+//The middleware will parse the Cookie header on the request and expose the cookie data as the property req.cookies and, if a secret was provided, as the property req.signedCookies. These properties are name value pairs of the cookie name to cookie value.
 app.use(bodyParser.json());
+//same but parsing the body
 app.use(bodyParser.urlencoded({ extended: true }));
+//url encoded so to distinguish ampersand
 app.use(helmet());
-app.use(morgan("dev"));
+//secure the HTTP headers
+app.use(morgan("dev")); //shows logs
 
 //routes
-app.use("/", globalRouter);
-app.use("/user", userRouter);
-app.use("/video", videoRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+app.use(routes.home, globalRouter);
 
 export default app;
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,17 @@
 import express from "express";
+import routes from "../routes.js";
+import {
+  users,
+  userDetail,
+  editProfile,
+  changePassword,
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
+
+userRouter.get("/", users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter;
