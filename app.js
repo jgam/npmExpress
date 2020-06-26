6bcdda62cb1a350f1c7fd4790a251ccde4d129d5
jgam <jgam@alumni.nd.edu>
2020-06-26 12:37:39 +0900

    [express] adding page title

--- app.js
+++ app.js
@@ -8,19 +8,23 @@ import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes.js";
+
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 
 const app = express();
 
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 //The middleware will parse the Cookie header on the request and expose the cookie data as the property req.cookies and, if a secret was provided, as the property req.signedCookies. These properties are name value pairs of the cookie name to cookie value.
 app.use(bodyParser.json());
 //same but parsing the body
 app.use(bodyParser.urlencoded({ extended: true }));
 //url encoded so to distinguish ampersand
-app.use(helmet());
 //secure the HTTP headers
 app.use(morgan("dev")); //shows logs
+app.use(localsMiddleware);
 
 //routes
 app.use(routes.users, userRouter);
