affb1f5b3451c03d0252ee4e47fe7a67efca3a95
jgam <jgam@alumni.nd.edu>
2020-07-23 23:44:16 +0900

    done

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -10,9 +10,8 @@ mongoose.connect(process.env.MONGO_URL, {
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅  Connected to DB");
-const handleError = (error) =>
-  console.log(`❌ Error on DB Connection:${error}`);
+const handleOpen = () => console.log('✅  Connected to DB');
+const handleError = (error) => console.log(`❌ Error on DB Connection:${error}`);
 
-db.once("open", handleOpen);
-db.on("error", handleError);
+db.once('open', handleOpen);
+db.on('error', handleError);
