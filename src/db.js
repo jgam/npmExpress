ae989a288e61c5e67392720425308ee940324187
jgam <jgam@alumni.nd.edu>
2020-07-24 23:20:48 +0900

    changed db

--- src/db.js
+++ src/db.js
@@ -4,7 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose.connect(
-  process.env.MONGO_URL,
+  //process.env.MONGO_URL,
+  "mongodb://localhost:27017/we-tube",
   {
     useNewUrlParser: true,
     useFindAndModify: false
